25d630bea5ae3fdf496064a305cacd5623cd9cc9
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-22 10:01:09 +0700

    Pengumpulan Tugas 24 JS - NIOMIC

---
+++ tugas24.js
@@ -0,0 +1,10 @@
+console.log(
+  "SORTING DAN REVERSE\n========================================================"
+);
+const myArray = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log("Sebelumnya :", myArray.toString());
+
+console.log("Ascending :", myArray.sort().toString());
+
+console.log("Descending :", myArray.reverse().toString());
